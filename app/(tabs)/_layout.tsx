3dc2875cbb8ec24c7ff6eb4321feca3bb7f17886
Abdelrahman Hatem <abdelrahmanhatem.dev@gmail.com>
2025-05-20 09:14:45 +0300

    update tabs button background

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,15 @@
 import { Tabs } from 'expo-router'
-import { ImageBackground } from 'react-native'
+import { Image, ImageBackground } from 'react-native'
 import images from '@/constants/images'
+import { icons } from '@/constants/icons'
 
 const _layout = () => {
   return (
    <Tabs>
     <Tabs.Screen  name="index" options={{title: "Home", headerShown: false, tabBarIcon: ({focused}) => (<>
-        <ImageBackground source={images.highlight}/>
+        <ImageBackground source={images.highlight} className='w-[50px] flex justify-center items-center p-5 rounded-lg'>
+            <Image source={icons.home}/>
+        </ImageBackground>
         </>) }}/>
     <Tabs.Screen  name="search" options={{title: "Search", headerShown: false }}/>
     <Tabs.Screen  name="saved" options={{title: "Saved", headerShown: false }}/>
